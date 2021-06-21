46b66742776b45d2dd9fd924fa47cca9fcc1e96d
linsalrob <raedwards@gmail.com>
2021-06-21 17:41:11 +0930

    updating setup.py for scm

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
-from distutils.core import Extension
+from distutils.core import setup, Extension
 import os
 
 # Read the markdown files for the long description
@@ -21,41 +21,36 @@ def get_version():
         return v
 
 
-def main():
-    setuptools.setup(
-        name='py_fasta_validator',
-        version=get_version(),
-        packages=setuptools.find_packages(),
-        url='https://github.com/linsalrob/py_fasta_validator',
-        license='The MIT License (MIT)',
-        author='Rob Edwards',
-        author_email='raedwards@gmail.com',
-        description='A small, lightweight, fast fasta validator written in C',
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        platforms='any',
-        keywords="bioinformatics genome genomics fasta",
-        ext_modules=[Extension("FastaValidator", sources=["src/fasta_validate.c"], language='c')],
-        entry_points={
-            "console_scripts": ["py_fasta_validator = PyFastaValidator.validate:validate"]
-        },
-        use_scm_version=True,
-        setup_requires=['setuptools_scm'],
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Operating System :: MacOS :: MacOS X',
-            'Operating System :: POSIX :: Linux',
-            'Operating System :: Unix',
-            'Programming Language :: Python :: 3.0',
-            'Topic :: Scientific/Engineering :: Bio-Informatics',
-        ],
-        zip_safe=True,
-    )
-
-
-if __name__ == "__main__":
-    main()
+setup(
+    name='py_fasta_validator',
+    version=get_version(),
+    packages=setuptools.find_packages(),
+    url='https://github.com/linsalrob/py_fasta_validator',
+    license='The MIT License (MIT)',
+    author='Rob Edwards',
+    author_email='raedwards@gmail.com',
+    description='A small, lightweight, fast fasta validator written in C',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    platforms='any',
+    keywords="bioinformatics genome genomics fasta",
+    ext_modules=[Extension("FastaValidator", sources=["src/fasta_validate.c"], language='c')],
+    entry_points={
+        "console_scripts": ["py_fasta_validator = PyFastaValidator.validate:validate"]
+    },
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.0',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    zip_safe=True,
+)
 
