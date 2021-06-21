591df2aad842b997b5a87bef94fb74c5a5ec42fd
linsalrob <raedwards@gmail.com>
2021-06-21 17:16:22 +0930

    correcting version source

--- PyFastaValidator/version.py
+++ PyFastaValidator/version.py
@@ -2,6 +2,6 @@ import pkg_resources
 # this comes from setuptools
 
 try:
-    __version__ = pkg_resources.get_distribution('PyFBA').version
+    __version__ = pkg_resources.get_distribution('py_fasta_validator').version
 except Exception:
     __version__ = 'unknown'
