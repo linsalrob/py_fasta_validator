20565b2ab0a5fd8f78e087bf5afbdbca41661dbe
Rob Edwards <raedwards@gmail.com>
2021-06-21 17:11:49 +0930

    Adding an entry point and version

--- PyFastaValidator/validate.py
+++ PyFastaValidator/validate.py
@@ -5,7 +5,7 @@ Validate a fasta file or except with the appropriate code.
 import sys
 import argparse
 
-from PyFastaValidator import validate_fasta_file
+from PyFastaValidator import validate_fasta_file, __version__
 
 
 def validate():
@@ -13,13 +13,17 @@ def validate():
     Validate a fasta file
     """
     parser = argparse.ArgumentParser(description='Validate a fasta file')
-    parser.add_argument('-f', help='fasta file to validate', required=True)
+    parser.add_argument('-f', '--file', help='fasta file to validate')
+    parser.add_argument('-v', '--version', help='print the version')
     args = parser.parse_args()
 
-    r = 0
-    try:
-        r = validate_fasta_file(args.f)
-    except Exception as e:
-        print(f"Recieved the error {e} from {type(e)}")
-    print(f"Exiting with code {r}")
-    sys.exit(r)
+    if args.file:
+        r = 0
+        try:
+            r = validate_fasta_file(args.f)
+        except Exception as e:
+            print(f"Recieved the error {e} from {type(e)}")
+        print(f"Exiting with code {r}")
+        sys.exit(r)
+    else:
+        print(f"{sys.argv[0]} version {__version__}")
